a8f22ed2d1449fdc618e164e3ac9d5641c61e938
diegoSilbestro <diegosilbestro@gmail.com>
2023-02-15 15:12:38 -0300

    Se elimina carpeta backend

--- Backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controllers = require('../controller/productController')
-
-/* GET home page. */
-router.get('/', controllers.myIndex);
-
-module.exports = router;
